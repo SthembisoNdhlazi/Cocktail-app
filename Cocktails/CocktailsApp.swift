cf7aff7615fabbf09e8e209db3b6fcf3038ae2f6
Sthembiso Ndhlazi <Sthembiso.Ndhlazi@vitality.co.uk>
2023-10-08 10:05:12 +0200

    Initial Commit

---
+++ Cocktails/CocktailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailsApp.swift
+//  Cocktails
+//
+//  Created by Sthembiso Ndhlazi on 2023/10/08.
+//
+
+import SwiftUI
+
+@main
+struct CocktailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
