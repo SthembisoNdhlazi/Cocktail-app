b23f19966e5bd9490b315728a803402a40a2f5ba
Sthembiso Ndhlazi <ndhlazisthe@gmail.com>
2023-10-31 12:51:24 +0200

    Added the reusable view for the SingleDrinkView

---
+++ Cocktails/Views/SingleDrinkView.swift
@@ -0,0 +1,72 @@
+//
+//  SingleDrinkView.swift
+//  Cocktails
+//
+//  Created by Sthembiso Ndhlazi on 2023/10/08.
+//
+
+import SwiftUI
+
+struct SingleDrinkView: View {
+    var drink: any Drinkable
+    var body: some View {
+        ZStack {
+            VStack {
+                if let imageURL = URL(string: drink.image) {
+                    AsyncImage(url: imageURL) { image in
+                        image
+                            .resizable()
+                            .frame(width: 250, height: 250)
+                            .mask(LinearGradient(gradient: Gradient(stops: [
+                                .init(color: .black, location: 0),
+                                .init(color: .clear, location: 1),
+                                .init(color: .black, location: 1),
+                                .init(color: .clear, location: 1)
+                            ]), startPoint: .top, endPoint: .bottom))
+                            .cornerRadius(15)
+                            .padding(.top)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                }
+                Spacer()
+            }
+            VStack {
+                Spacer()
+                HStack {
+                    Text(drink.drinkName)
+                        .padding()
+                    Spacer()
+                }
+                if let glass = drink.glass {
+                    HStack {
+                        Text(glass)
+                            .padding()
+                        Spacer()
+                    }
+                }
+                
+                if let category = drink.category {
+                    HStack {
+                        Text(category)
+                            .padding()
+                        Spacer()
+                    }
+                }
+            }
+            
+        }
+        .frame(width: 350, height:450)
+        .overlay {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(.gray, lineWidth: 5)
+        }
+    }
+}
+
+struct SingleDrinkView_Previews: PreviewProvider {
+    static var previews: some View {
+        var selectedDrink: Drink = Drink(drinkName: "Mojito", glass: "Normal glass", instructions: "Just drink", image: "https://cdn.loveandlemons.com/wp-content/uploads/2020/07/mojito.jpg", category: "Extremely alcoholic")
+        SingleDrinkView(drink: selectedDrink)
+    }
+}
